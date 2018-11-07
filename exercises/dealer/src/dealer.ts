a5f42e9122f19f04b6f93177d372ea64ae027754
nickfoden <nickfoden@gmail.com>
2018-11-07 17:06:25 -0500

    In middle of making a card deck from enums challenge

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,57 @@ function shuffleArray(a: any[]) {
   }
 }
 
-// export class Dealer {
+export enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades
+}
 
-// }
+export enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+}
+
+type Card = [Suit, CardNumber];
+
+let c: Card = [Suit.Clubs, CardNumber.Seven];
+
+function createDeck(): Card[] {
+  let cards: Card[] = [];
+  for (let s = 0; s < Object.keys(Suit).length; s += 2) {
+    for (let n = 0; n < Object.keys(CardNumber).length; n += 2) {
+      cards.push([s / 2, n / 2]);
+    }
+  }
+  return cards;
+}
+
+export class Dealer {
+  cards: Card[] = [];
+  constructor() {
+    this.cards = createDeck();
+    shuffleArray(this.cards);
+  }
+
+  dealHand(numCards: number): Card[] {}
+
+  getLength(): number {
+    return this.cards.length;
+  }
+  readCard(card: Card): string {}
+}
+
+let d = new Dealer();
+console.log(d.getLength());
