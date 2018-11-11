02319bbda0afc6325ae0ac51521f0b847dc5c974
Nick Foden <nickfoden@gmail.com>
2018-11-11 14:10:07 -0500

    Had to pass an empty object to super call to compile jsx statefull components

--- exercises/autocomplete-sfc/src/app.tsx
+++ exercises/autocomplete-sfc/src/app.tsx
@@ -8,28 +8,22 @@ interface IAppState {
 
 export class App extends React.Component<{}, IAppState> {
   constructor() {
-    super();
+    super({});
     this.state = {
       results: []
     };
   }
   async componentDidMount() {
-    let placeSummaries: PlaceSummary[] = await fetchPlaceSummaries('donut');
+    let placeSummaries: PlaceSummary[] = await fetchPlaceSummaries('doughnut');
     let results: PlaceDetails[] = await fetchPlaceDetails(placeSummaries.map(p => p.place_id));
+
     this.setState({ results });
   }
   render() {
     console.log(this.state.results);
     let placeResults = this.state.results.map(pr => {
-      /////////////////////////////////////////////
-      // 👇 Replace this with your new <PlaceSearchResult /> component //
-      return <p key={pr.id}>{pr.name}</p>;
-      // return <PlaceSearchResult {...pr}/>;
+      return <PlaceSearchResult {...pr} />;
     });
-    return (
-      <ul className='results'>
-        {placeResults}
-      </ul>
-    );
+    return <ul className="results">{placeResults}</ul>;
   }
-};
+}
